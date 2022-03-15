29b11c74ba1884d4f24f4d66e4b3bb0b3a04396d
mattisevitt <mattisevitt@gmail.com>
2022-03-15 20:59:15 +0200

    finshed all questions

--- IMLearn/learners/gaussian_estimators.py
+++ IMLearn/learners/gaussian_estimators.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 import numpy as np
 from numpy.linalg import inv, det, slogdet
+from numpy import transpose
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 class UnivariateGaussian:
@@ -89,8 +93,7 @@ class UnivariateGaussian:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
         PDF = np.ndarray(X.size)
         for i in range(X.size):
-            mone = np.exp((-(X[i]-(self.mu_**2))) / (2 * self.var_))
-            PDF[i] = mone / np.sqrt(2 * np.pi * self.var_)
+            PDF[i] = np.exp((-(X[i]-(self.mu_))**2) / (2 * self.var_)) / np.sqrt(2 * np.pi * self.var_)
         return PDF
 
 
@@ -170,8 +173,8 @@ class MultivariateGaussian:
         X should be of shape (n_samples, n_features)
         """
         #
-        self.cov_ = np.cov(X, bias=False, ddof=1)
-        self.mu_ = np.mean(X, axis=1)
+        self.cov_ = np.cov(X, bias=False, ddof=1, rowvar=False)
+        self.mu_ = np.mean(X, axis=0)
         self.fitted_ = True
         return self
 
@@ -196,29 +199,75 @@ class MultivariateGaussian:
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
         d = X[0].size
-        inverse = np.linalg.inv(self.cov_)
-        return 1 / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(self.cov_)) * \
-               np.exp((-1 / 2) * (X - self.mu_).transpose().dot(inverse).dot((X - self.mu)))
+        inverse = inv(self.cov_)
+        return 1 / np.sqrt(((2 * np.pi) ** d) * det(self.cov_)) * \
+               np.exp((-0.5) * (X - self.mu_).sum().dot(inverse).dot((X - self.mu)))
+
+
+def log_likelihood(mu: np.ndarray, cov: np.ndarray, X: np.ndarray) -> float:
+    """
+    Calculate the log-likelihood of the data under a specified Gaussian model
+
+    Parameters
+    ----------
+    mu : float
+        Expectation of Gaussian
+    cov : float
+        covariance matrix of Gaussian
+    X : ndarray of shape (n_samples, )
+        Samples to calculate log-likelihood with
+
+    Returns
+    -------
+    log_likelihood: float
+        log-likelihood calculated
+    """
+    m = X.shape[0]
+    md = X.size
+    B = 0
+    for i in range(m):
+        B += np.linalg.multi_dot([(X[i] - mu).transpose(), inv(cov), (X[i] - mu)])
+    A = md*np.log(2*np.pi) + m*np.log(det(cov))
+    return -0.5 * (A + B )
+
+
+if __name__ == '__main__':
+    # q1
+    mu1 = 10
+    sigma1 = 1
+    X1 = np.random.normal(mu1, sigma1, 1000)
+    UG = UnivariateGaussian()
+    UG.fit(X1)
+    print("expectation1: ", UG.mu_, "variance1: ", UG.var_, "\n")
+    arr = np.ndarray(100)
+    for i, smp in enumerate(range(10, 1001, 10)):
+        temp = UnivariateGaussian()
+        temp.fit(X1[:smp])
+        arr[i] = np.abs(temp.mu_ - mu1)
+    # q2
+    px.scatter( x= np.array(list(range(10,1001, 10))), y = arr, title="question 2").update_xaxes\
+        (title_text="sample size").update_yaxes(title_text="|estimated - true value of expectation|").show()
+    # # q3
+    px.scatter(x=X1, y=UG.pdf(X1), title="question 3").update_xaxes\
+        (title_text="ordered sample values").update_yaxes(title_text="PDF").show()
+    # q4
+    mu2 = np.array([0,0,4,0])
+    cov_matrix = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    X2 = np.random.multivariate_normal(mu2, cov_matrix, 1000)
+    MG = MultivariateGaussian()
+    MG.fit(X2)
+    print("expectation2: \n", MG.mu_, "\n", "covar matrix: \n", MG.cov_)
+    # q5
+    f1 = np.linspace(-10, 10, 200)
+    f3 = np.linspace(-10, 10, 200)
+    log_matrix = np.array([[log_likelihood(np.array([f1[i], 0, f3[j], 0]), cov_matrix, X2)
+                            for j in range(200)] for i in range(200)])
+    px.imshow(log_matrix, x=f1, y=f3, labels=dict(x="f1", y="f3", color="log-likelihood"), title="question 5").show()
+    # q6
+    max_val = np.amax(log_matrix)
+    max_location = np.where(log_matrix == max_val)
+    print("max log-likelihood value: ", max_val, "f1 index: ", max_location[0][0], "f3 index: ", max_location[1][0])
 
 
 
-    @staticmethod
-    def log_likelihood(mu: np.ndarray, cov: np.ndarray, X: np.ndarray) -> float:
-        """
-        Calculate the log-likelihood of the data under a specified Gaussian model
 
-        Parameters
-        ----------
-        mu : float
-            Expectation of Gaussian
-        cov : float
-            covariance matrix of Gaussian
-        X : ndarray of shape (n_samples, )
-            Samples to calculate log-likelihood with
-
-        Returns
-        -------
-        log_likelihood: float
-            log-likelihood calculated
-        """
-        raise NotImplementedError()
